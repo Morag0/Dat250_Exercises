f1da22cebb680f45833697d79ae55975ca16cf1d
Morag0 <morten.aagotnes@gmail.com>
2024-10-04 18:14:32 +0200

    Done with assigment 6

---
+++ dat250-expass6.md
@@ -0,0 +1,9 @@
+I had no technical problems or issues with this assigment.
+
+The code can be found under src/main/java/rabbitmq.
+
+Send.java and Receive.java has the code for Experiment 2
+
+NewTask.java and Worker.java has the code for Experiment 3
+
+EmitLog.java and ReceiveLogs.java has the code for Experiment 4
--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -19,8 +19,10 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.rabbitmq:amqp-client:5.16.0")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
 }
 
 tasks.withType<Test> {
